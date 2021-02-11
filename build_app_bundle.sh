19ecbe005aae9cef2d8d233c2f719235a08a9cb8
emlys <esoth@stanford.edu>
2021-02-11 10:33:10 -0700

    move app out of subdirectory

--- build_app_bundle.sh
+++ build_app_bundle.sh
@@ -4,7 +4,7 @@
 
 # Make the app bundle
 mkdir -p "build/mac_app/Example.app/Contents/MacOS"
-cp -r "dist/example" "build/mac_app/Example.app/Contents/MacOS/example_dist"
+cp -r "dist/example_" "build/mac_app/Example.app/Contents/MacOS/"
 cp "Info.plist" "build/mac_app/Example.app/Contents/Info.plist"
 
 
@@ -15,6 +15,6 @@ echo '#' >> $executable_file
 echo '# the QT_MAC_WANTS_LAYER definition is supposed to have been set by the' >> $executable_file
 echo "# runtime hook, but doesn't seem to be working.  Setting it here allows the" >> $executable_file
 echo "# binary to run on OSX Big Sur." >> $executable_file
-echo 'QT_MAC_WANTS_LAYER=1 `dirname $0`/example_dist/example launch' >> $executable_file
+echo 'QT_MAC_WANTS_LAYER=1 `dirname $0`/example_ launch' >> $executable_file
 chmod a+x $executable_file
 
