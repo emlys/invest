a466994a65ae378284ed755254950259d5499622
Emily Soth <esoth@stanford.edu>
2025-02-24 13:34:49 -0800

    debugging

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -342,7 +342,9 @@ class DirectoryValidationMacOnly(unittest.TestCase):
         from natcap.invest import validation
 
         with tempfile.TemporaryDirectory() as tempdir:
+            print(os.stat(tempdir))
             os.chmod(tempdir, stat.S_IREAD)
+            print(os.stat(tempdir))
             validation_warning = validation.check_directory(tempdir,
                                                             permissions='rwx')
             self.assertEqual(
