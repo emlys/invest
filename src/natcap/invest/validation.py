60a88be94c38f209d3f340dc1f04c2d10a0558e0
Emily Soth <esoth@stanford.edu>
2025-02-24 13:55:35 -0800

    debugging

--- src/natcap/invest/validation.py
+++ src/natcap/invest/validation.py
@@ -203,9 +203,8 @@ def check_directory(dirpath, must_exist=True, permissions='rx', **kwargs):
     # Check for x access before checking for w,
     # since w operations to a dir are dependent on x access
     if 'x' in permissions:
-        print(bool(os.stat(dirpath).st_mode & stat.S_IRUSR))
-        print(bool(os.stat(dirpath).st_mode & stat.S_IWUSR))
-        print(bool(os.stat(dirpath).st_mode & stat.S_IXUSR))
+        print(os.stat(dirpath))
+
         try:
             cwd = os.getcwd()
             os.chdir(dirpath)
