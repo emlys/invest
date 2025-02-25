dd4c3cb54db4789b5a7603ffe13237d699377ceb
Emily Soth <esoth@stanford.edu>
2025-02-24 16:44:06 -0800

    debugging

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -343,7 +343,11 @@ class DirectoryValidationMacOnly(unittest.TestCase):
         from natcap.invest import validation
 
         with tempfile.TemporaryDirectory() as tempdir:
+            result = subprocess.run(['ls', '-ld', tempdir], capture_output=True, text=True)
+            print(result.stdout)
             subprocess.run(['chmod', '0444', tempdir])
+            result = subprocess.run(['ls', '-ld', tempdir], capture_output=True, text=True)
+            print(result.stdout)
             # os.chmod(tempdir, stat.S_IREAD)
 
             validation_warning = validation.check_directory(tempdir,
